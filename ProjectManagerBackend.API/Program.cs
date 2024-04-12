273a6007fedef183707dbcfffc9e932a7fe6ae58
Jonas Østergaard <jona339e@elev.tec.dk>
2024-04-12 12:24:47 +0200

    Base project

--- ProjectManagerBackend.API/Program.cs
+++ ProjectManagerBackend.API/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectManagerBackend.Repo.Data;
+using ProjectManagerBackend.Repo.Interfaces;
+using ProjectManagerBackend.Repo.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Connection
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// DI
+
+builder.Services.AddScoped<ICrudInterface, ProjectRepository>();
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ProjectManagerBackend.Repo/Repositories/ProjectRepository.cs
+++ ProjectManagerBackend.Repo/Repositories/ProjectRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectManagerBackend.Repo.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProjectManagerBackend.Repo.Repositories
 {
-    public interface ProjectRepository
+    public class ProjectRepository: ICrudInterface
     {
     }
 }
