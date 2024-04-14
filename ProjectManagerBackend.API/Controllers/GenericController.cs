6c5d3fa2f10e86665f4b75549f7b64a893889e88
Jonas Østergaard <Jonasoe@hotmail.com>
2024-04-14 10:55:35 +0200

    Generic Controller

---
+++ ProjectManagerBackend.API/Controllers/GenericController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagerBackend.Repo.Interfaces;
+
+namespace ProjectManagerBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenericController<TEntity> : ControllerBase where TEntity : class
+    {
+        private readonly IGenericRepository<TEntity> _repository;
+
+        public GenericController(IGenericRepository<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TEntity>>> GetAll()
+        {
+            var items = await _repository.GetAll();
+            if (items == null)
+            {
+                return NotFound();
+            }
+            return Ok(items);
+        }
+    }
+}
