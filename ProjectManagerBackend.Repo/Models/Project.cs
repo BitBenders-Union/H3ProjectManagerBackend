bf27a53f0efc3d49c38df3d7888691a914340aab
Jonas Østergaard <jona339e@elev.tec.dk>
2024-04-15 13:15:44 +0200

    Generic in progress

--- ProjectManagerBackend.Repo/Models/Project.cs
+++ ProjectManagerBackend.Repo/Models/Project.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ProjectManagerBackend.Repo.Models
@@ -12,11 +13,11 @@ namespace ProjectManagerBackend.Repo.Models
         public string Name { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
-        public ProjectStatus ProjectStatus { get; set; }
-        public List<ProjectTask> ProjectTasks { get; set; }
-        public ProjectCategory ProjectCategory { get; set; }
-        public Priority Priority { get; set; }
-        public Client Client { get; set; }
-        public List<Department> Departments { get; set; }
+        public ProjectStatus? ProjectStatus { get; set; }
+        public List<ProjectTask>? ProjectTasks { get; set; }
+        public ProjectCategory? ProjectCategory { get; set; }
+        public Priority? Priority { get; set; }
+        public Client? Client { get; set; }
+        public List<Department>? Departments { get; set; }
     }
 }
