1ccba0630be6452c9e49bfa6d9a7c27b5324a213
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-04-17 10:13:03 +0200

    Added update to most controllers
    Co-Authored-By: Jonas Østergaard <98744422+jona339e@users.noreply.github.com>

--- ProjectManagerBackend.API/Controllers/DepartmentController.cs
+++ ProjectManagerBackend.API/Controllers/DepartmentController.cs
@@ -7,11 +7,34 @@ namespace ProjectManagerBackend.API.Controllers
     [ApiController]
     public class DepartmentController : GenericController<Department, DepartmentDTO, DepartmentDTO>
     {
+        private readonly IMappingService _mappingService;
         public DepartmentController(
             IGenericRepository<Department> repository,
             IMappingService mapping
             ) : base(repository, mapping)
         {
+            _mappingService = mapping;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(DepartmentDTO department)
+        {
+            try
+            {
+                if (department == null)
+                {
+                    return BadRequest("Department cannot be null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Invalid model state");
+                }
+                return Ok(await _repository.UpdateAsync(_mapping.Map<DepartmentDTO, Department>(department)));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
