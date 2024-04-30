846f25efbc68d3699a3f8bb324957e87e9bdaaef
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-04-30 08:38:48 +0200

    Added projectStatus

---
+++ ProjectManagerBackend.API/Controllers/ProjectStatusController.cs
@@ -0,0 +1,42 @@
+﻿namespace ProjectManagerBackend.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectStatusController : GenericController<ProjectStatus, ProjectStatusDTO, ProjectStatusDTO>
+    {
+        private readonly IMappingService _mappingService;
+
+        public ProjectStatusController(
+            IGenericRepository<ProjectStatus> repository,
+            IMappingService mapping) : base(repository, mapping)
+        {
+            _mappingService = mapping;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProjectStatusDTO projectStatusDTO)
+        {
+            try
+            {
+                if (projectStatusDTO == null)
+                {
+                    return BadRequest("Project Status cannot be null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Invalid model state");
+                }
+
+                return Ok(await _repository.UpdateAsync(_mapping.Map< ProjectStatusDTO, ProjectStatus>(projectStatusDTO)));
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
